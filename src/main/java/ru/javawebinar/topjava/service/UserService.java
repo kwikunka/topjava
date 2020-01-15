ef997dfd87ba32a7d7345e5cecc04297703aaa9f
Ilya Smelov <smelov.ilya@gmail.com>
2020-01-15 17:44:21 +0300

    add JUnit, DB, JDBC

--- src/main/java/ru/javawebinar/topjava/service/UserService.java
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
@@ -14,9 +15,10 @@ import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 @Service
 public class UserService {
 
+    @Autowired
+    @Qualifier("jdbcUserRepository")
     private final UserRepository repository;
 
-    @Autowired
     public UserService(UserRepository repository) {
         this.repository = repository;
     }
